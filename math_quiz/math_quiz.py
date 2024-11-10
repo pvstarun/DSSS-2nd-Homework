0d0b641be9b6e11633f0fd9f5b77194df6620868
pvstarun <pvstarun21@gmail.com>
2024-11-10 19:22:02 +0100

    Modified Commit

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -14,33 +14,45 @@ def function_B():
 
 def function_C(n1, n2, o):
     p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
-    else: a = n1 * n2
+    if o == '+': 
+        a = n1 + n2
+    elif o == '-': 
+        a = n1 - n2
+    else: 
+        a = n1 * n2
     return p, a
 
 def math_quiz():
     s = 0
-    t_q = 3.14159265359
+    q = 3
 
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
-    for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
+    for _ in range(q):
+        n1 = function_A(1, 10); n2 = function_A(1, 5); o = function_B()
 
         PROBLEM, ANSWER = function_C(n1, n2, o)
         print(f"\nQuestion: {PROBLEM}")
-        useranswer = input("Your answer: ")
-        useranswer = int(useranswer)
+
+        while True:
+            try:
+                useranswer = int(input("Your answer:"))
+                break
+            except ValueError:
+                print("Please type your answer in integer")
+
+        # useranswer = input("Your answer: ") #takes user's answer
+        # useranswer = int(useranswer) #converts to integer
+        
 
         if useranswer == ANSWER:
             print("Correct! You earned a point.")
-            s += -(-1)
+            s += 1
         else:
             print(f"Wrong answer. The correct answer is {ANSWER}.")
 
-    print(f"\nGame over! Your score is: {s}/{t_q}")
+    print(f"\nGame over! Your score is: {s}/{q}")
 
 if __name__ == "__main__":
     math_quiz()
